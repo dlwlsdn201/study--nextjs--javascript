d7db204b144ea0697e8c24f56d348e099bea9b37
leejinw <leejinw@nxtcorp.co.kr>
2022-08-14 14:28:32 +0900

    :sparkle: 1. Nav 스타일 개선 + 2. open API 로 영화 데이터 fetch

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,6 +6,7 @@ export default function NavBar() {
 	const router = useRouter();
 	return (
 		<nav>
+			<img src='/vercel.svg' alt='logo' />
 			<Link href='/'>
 				<a className={router.pathname === '/' ? 'active' : ''}>Home</a>
 			</Link>
@@ -13,9 +14,33 @@ export default function NavBar() {
 				<a className={router.pathname === '/about' ? 'active' : ''}>about</a>
 			</Link>
 			<style jsx>{`
+				a {
+					text-decoration: none;
+				}
+				nav {
+					display: flex;
+					gap: 10px;
+					flex-direction: column;
+					align-items: center;
+					padding-top: 20px;
+					padding-bottom: 10px;
+					box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+						rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+				}
+				img {
+					max-width: 100px;
+					margin-bottom: 5px;
+				}
+				nav a {
+					font-weight: 600;
+					font-size: 18px;
+				}
 				.active {
-					padding: 4px;
-					color: red;
+					color: tomato;
+				}
+				nav div {
+					display: flex;
+					gap: 10px;
 				}
 			`}</style>
 		</nav>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-import Head from 'next/head';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Seo from './Seo';
 
 export default function Home() {
+	const API_KEY = 'aad519b83c9fdb2b8aad15936da11445';
+	const [movies, setMovies] = useState([]);
+	const BASE_URL = `https://api.themoviedb.org/3`;
+	const url = `/movie/popular?api_key=${API_KEY}`;
+	useEffect(() => {
+		(() =>
+			axios({
+				method: 'GET',
+				baseURL: BASE_URL,
+				timeout: 1000 * 20,
+				url
+			}).then((result) => {
+				setMovies(result.data.results);
+			}))();
+	}, []);
+
 	return (
 		<>
 			<Seo title='Home' />

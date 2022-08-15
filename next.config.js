a04c59b6f606460603afc7dc42e558a5cb6eedd0
leejinw <leejinw@nxtcorp.co.kr>
2022-08-15 16:21:49 +0900

    :sparkle: redirect 와 rewrite 기능을 통해 api key 가리는 기능 구현

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+
+	const API_KEY = 'aad519b83c9fdb2b8aad15936da11445';
+
+	const nextConfig = {
+		reactStrictMode: true,
+		swcMinify: true,
+		async redirects() {
+			// 바뀐 new url (destination) 을 사용자가 브라우저 주소창을 통해 알 수 있음
+			return [
+				{
+					source: '/contract', // 사용자가 접근(요청)하려는 표면상 url
+					destination: 'https://www.google.com', // 실제 사용자를 redirection 할 url
+					permanent: false // 브라우저나 검색엔진이 이 정보를 기억하는지 여부
+				},
+				{
+					source: '/old-blog/:path', // pattern-matching : 앞에 주소만 바뀌고, 뒤에 쿼리 (:path) 는 그대로 유지
+					destination: '/new-blog/:path',
+					permanent: false
+				},
+				{
+					source: '/old-blog/:path*', // * : 쿼리 뒤에 어떠한 것들이 추가적으로 붙을 수 있다는 뜻.
+					destination: '/new-blog/:path*',
+					permanent: false
+				}
+			];
+		},
+		async rewrites() {
+			// 바뀐 new url (destination) 을 사용자가 통해 알 수 없음. 즉, source url 이 그대로 사용자에게 보이지만, 실제 연결은 destination 사이트에 연결됨.
+			return [
+				{
+					source: '/api/movies', //source 는 무조건 '/' 으로 시작해야한다.
+					destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+				}
+			];
+		}
+	};
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ import Seo from './Seo';
 
 export default function Home() {
 	const API_KEY = 'aad519b83c9fdb2b8aad15936da11445';
-	const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState();
 	const BASE_URL = `https://api.themoviedb.org/3`;
 	const url = `/movie/popular?api_key=${API_KEY}`;
 	useEffect(() => {
 		(() =>
 			axios({
 				method: 'GET',
-				baseURL: BASE_URL,
+				// baseURL: BASE_URL,
 				timeout: 1000 * 20,
-				url
+				url: '/api/movies'
 			}).then((result) => {
 				setMovies(result.data.results);
 			}))();
@@ -21,8 +21,40 @@ export default function Home() {
 
 	return (
 		<>
-			<Seo title='Home' />
-			<div>Hello</div>
+			<div className='container'>
+				<Seo title='Home' />
+				{movies?.map((movie) => (
+					<div key={movie.id}>
+						<div className='movie' key={movie.id}>
+							<img
+								src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+							/>
+							<h4>{movie.original_title}</h4>
+						</div>
+					</div>
+				))}
+				<style jsx>{`
+					.container {
+						display: grid;
+						grid-template-columns: 1fr 1fr;
+						padding: 20px;
+						gap: 20px;
+					}
+					.movie img {
+						max-width: 100%;
+						border-radius: 12px;
+						transition: transform 0.2s ease-in-out;
+						box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+					}
+					.movie:hover img {
+						transform: scale(1.05) translateY(-10px);
+					}
+					.movie h4 {
+						font-size: 18px;
+						text-align: center;
+					}
+				`}</style>
+			</div>
 		</>
 	);
 }

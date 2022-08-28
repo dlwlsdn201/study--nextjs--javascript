019d8bb4f3b493c0022a70e59205ae3e27163069
leejinw <leejinw@nxtcorp.co.kr>
2022-08-28 12:11:08 +0900

    :feature: getServerSideProps() 을 적용하여 SSR 기능 활성화2

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
+
+// pageProps : 각 페이지 컴포넌트의 getServerSideProps() 함수에서 return 되는 { props: {...} } 객체임. 즉, 해당 페이지로 props 라는 인자로 전달됨.
+
 export default function App({ Component, pageProps }) {
 	return (
 		<>
